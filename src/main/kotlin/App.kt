9b8fd45795a49e8923045ffbf78df4432400b652
BlackPantific <blackpantific@gmail.com>
2022-07-17 16:45:44 +0300

    Initial commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    var size = args.size
+}
+
+fun foo(firstParam: Int, secParam: Int): Int{
+    return if(firstParam > secParam){
+        firstParam
+    }else{
+        secParam
+    }
+}
+
+fun foo1(firstParam: Int, secParam: Int): Unit{// возвращает void
+
+}
