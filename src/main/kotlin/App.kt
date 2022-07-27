a36528d83026c0c686b2da9d2d0c9b726540ad82
BlackPantific <blackpantific@gmail.com>
2022-07-27 19:40:11 +0300

    inline

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,79 +1,37 @@
 
 fun main(args: Array<String>) {
 
-    //вызов с именованными параметрами
-    funWithOneExpression(arg2 = 5, arg1 = 7)
+    var res = funcWithFuncInParam(
+        value = 5,
+        someString = "String",
+        function = { param1: Int, param2: Double ->
+            param1.toString() + param2.toString()
+        },
+    )
 
-    //функ крешает при вызове
-    //exampleFunWithNothingType()
+    //Сокращенный синтаксис можно использовать, только когда лямбда передается в функцию как последний аргумент.
+    "Mississippi".count({ it == 's' })
 
-    //объявление анонимной функции без параметров
-    val greetingFunction: () -> String = {
+    "Mississippi".count { it == 's' }
+
+    funcWithFuncInParam(5, { someInt, someDouble ->
+        someInt.toString() + someDouble.toString()
+    }, "5")
+
+    runSimulation("Guyal") { playerName, numBuildings ->
         val currentYear = 2018
-        //анонимная функция позволяет не использовать оператор return
-        "Welcome to SimVillage, Mayor! (copyright $currentYear)"
-    }
-    //можно инвокнуть функцию так
-    var str = greetingFunction.invoke()
-    //или так
-    var stri = greetingFunction()
-
-    //анонимная функция с использованием ключевого слова it, которое может употребляться вместо первого параметра
-    val greetingFunction1: (String) -> String = {
-        val currentYear = 2018
-        "Welcome to SimVillage, $it! (copyright $currentYear)"
-    }
-    println(greetingFunction1("Guyal"))
-
-
-    //анон функция с несколькими параметрами
-    val anonymousFuncWithParameters: (String, Double) -> String = { someParam, doubleParam ->
-        val c = 5
-        val e = 3
-        someParam + c.toString() + e.toString() + doubleParam.toString()
+        println("Adding $numBuildings houses")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 
-    println({
-        val year = 2018
-        "Welcome to SimVillage, Mayor! (copyright $year)"
-    })
-
-    var anonFunc = { param1: Int, param2: Int ->
-        var someVar = param1 + 1
-        var someNewVar = param2 + 1
-    }
 }
 
-//по-умолчанию у всех фунций доступ public при объявлении
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean):
-        String {
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds, but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
-    return healthStatus
+inline fun funcWithFuncInParam(value: Int, function: (Int, Double) -> String, someString: String) : String{
+    return value.toString() + function.invoke(5, 7.0)
 }
 
-//аргумент по-умолчанию
-private fun castFireball(numFireballs: Int = 2) {
-    println("A glass of Fireball springs into existence. (x$numFireballs)")
-}
-
-//функция с одним выражением
-fun funWithOneExpression(arg1: Int, arg2: Int) : Unit = println("Hello world!")
-
-//использование типа Nothing
-fun exampleFunWithNothingType() : Nothing {
-    var m = 4
-    var res = "res"
-    println("Hello, this function will crash...")
-    //TODO()
-    throw NotImplementedError()
+inline fun runSimulation(playerName: String,
+                         greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last() // Случайно выберет 1, 2 или 3
+    println(greetingFunction(playerName, numBuildings))
 }
