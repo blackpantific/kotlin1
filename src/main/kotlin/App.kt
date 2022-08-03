794fa4a301f558155ce6fe0d64efff4f2cbf6fa3
BlackPantific <blackpantific@gmail.com>
2022-08-03 16:41:16 +0300

    inheritance

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,7 +1,4 @@
 fun main(args: Array<String>) {
 
-    val sword = Sword("Excalibur")
-    println(sword.name)
-    sword.name = "Gleipnir"
-    println(sword.name)
+
 }
---
+++ src/main/kotlin/Room.kt
@@ -0,0 +1,28 @@
+open class Room(val name: String) {
+    protected open val dangerLevel = 5
+
+    fun description() = "Room: $name\n" +
+            "Danger level: $dangerLevel"
+
+    open fun load() = "Nothing much to see here..."
+
+//    constructor(value: Int, db: Double) : this("defString") {
+//
+//    }
+}
+
+class TownSquare : Room("Town Square"){
+
+    override val dangerLevel = super.dangerLevel - 3
+    private var bellSound = "GWONG"
+
+    final override fun load() =
+        "The villagers rally and cheer as you enter!\n${ringBell()}"
+
+    private fun ringBell() = "The bell tower announces your arrival. $bellSound"
+
+//    constructor() : super(5, 7.0){
+//        println("hello world!")
+//    }
+
+}
