8903e1c5ded59c03380d36c5292ec44f80b47311
BlackPantific <blackpantific@gmail.com>
2022-08-04 13:07:26 +0300

    some kind of event

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,32 +1,47 @@
 fun main(args: Array<String>) {
 
-    var value = Direction.NORTH
-    if (value == Direction.NORTH) {
-        var res = Direction.NORTH.updateCoordinate(Coordinate(10, 20))
+    var myClass = MyClassWithDelegate()
+    myClass.someEvent = {int, double ->
+            "dgfgf"
+        }
+
+}
+
+//interface Fightable {
+//    var healthPoints: Int
+//    val diceCount: Int
+//    val diceSides: Int
+//    val damageRoll: Int
+//    fun attack(opponent: Fightable): Int
+//}
+
+public class ClassThatInvokes{
+
+    var someInt: Int
+    var someDouble: Double
+
+    constructor(int: Int, double: Double){
+        someInt = int
+        someDouble = double
+//        MyClassWithDelegate.someEvent = {int, double ->
+//            "dgfgf"
+//        }
     }
 }
 
-enum class Direction(private val coordinate: Coordinate) {
-    NORTH(Coordinate(0, -1)),
-    EAST(Coordinate(1, 0)),
-    SOUTH(Coordinate(0, 1)),
-    WEST(Coordinate(-1, 0));
+public class MyClassWithDelegate{
+    var someValue: Int? = null
 
-    fun updateCoordinate(playerCoordinate: Coordinate) =
-        Coordinate(
-            playerCoordinate.x + coordinate.x,
-            playerCoordinate.y + coordinate.y
-        )
+    var someString: String? = null
 
-    //с перегрузкой оператора можно делать так
-//    fun updateCoordinate(playerCoordinate: Coordinate) =
-//        coordinate + playerCoordinate
-}
+//    companion object{
+//        lateinit var someEvent: (val1: Int, val2: Int) -> String
+//    }
 
-data class Coordinate(val x: Int, val y: Int) {
-    val isInBounds = x >= 0 && y >= 0
+    lateinit var someEvent: (val1: Int, val2: Int) -> String
 
-    //в Kotlin можно перегружать следующие операторы:
-    //+ += == > [] .. in и другие
-    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
+    fun invokeDelegate(){
+        println("Working with some process...")
+        var res = someEvent.invoke(5,7)
+    }
 }
