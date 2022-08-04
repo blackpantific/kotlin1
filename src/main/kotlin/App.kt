900c8b0c1da73e98a6a0aed308427050b927d004
BlackPantific <blackpantific@gmail.com>
2022-08-04 12:42:17 +0300

    operator overload

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,8 @@
 fun main(args: Array<String>) {
 
     var value = Direction.NORTH
-    if(value == Direction.NORTH)
-    {
-        var res = Direction.NORTH.updateCoordinate(Coordinate(10,20))
+    if (value == Direction.NORTH) {
+        var res = Direction.NORTH.updateCoordinate(Coordinate(10, 20))
     }
 }
 
@@ -14,10 +13,20 @@ enum class Direction(private val coordinate: Coordinate) {
     WEST(Coordinate(-1, 0));
 
     fun updateCoordinate(playerCoordinate: Coordinate) =
-        Coordinate(playerCoordinate.x + coordinate.x,
-            playerCoordinate.y + coordinate.y)
+        Coordinate(
+            playerCoordinate.x + coordinate.x,
+            playerCoordinate.y + coordinate.y
+        )
+
+    //с перегрузкой оператора можно делать так
+//    fun updateCoordinate(playerCoordinate: Coordinate) =
+//        coordinate + playerCoordinate
 }
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    //в Kotlin можно перегружать следующие операторы:
+    //+ += == > [] .. in и другие
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
