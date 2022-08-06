393e459475d93df4737cb2c3fc4f73ff77f81415
BlackPantific <blackpantific@gmail.com>
2022-08-06 16:11:50 +0300

    out and in modifiers with explanation

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,52 +1,61 @@
+import java.util.Base64
+
 fun main(args: Array<String>) {
 
-    val lootBoxOne: LootBox<Fedora> = LootBox(
-        Fedora(
-            "a generic-looking fedora",
-            15
-        ),
-        Fedora("a dazzling magenta fedora", 25)
-    )
-    val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
-    lootBoxOne.open = true
-    lootBoxOne.fetch(1)?.run {
-        println("You retrieve $name from the box!")
-    }
-
-    //можно использовать перегрузку операторов вместо кода ниже
-//    val coin = lootBoxOne.fetch(0) {
-//        Coin(it.value * 3)
-//    }
-//    coin?.let { println(it.value) }
-
-    val fedora = lootBoxOne[1]
-    fedora?.let { println(it.name) }
-
-//    val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
-//    lootBoxOne.fetch()?.run {
-//        println("You retrieve $name from the box!")
-//    }
+//    var a: Generic<A> = Generic(A("1"))
+//    var b: Generic<Base> = Generic(B(1))
 //
-//    val coin = lootBoxOne.fetch() {
-//        Coin(it.value * 3)
-//    }
-//    coin?.let { println(it.value) }
+//    var c: Generic<Base> = b
+//
+//    var res = c.item as A
+//
+//    var a1 = 5
+
+    var a: Generic<A> = Generic(A("1"))
+    var b: Generic<Base> = Generic(B(1))
+
+    b = a
+
+    var fedoraBarrel: Barrel<Fedora> =
+        Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Loot(10))
+
+    fedoraBarrel = lootBarrel
+
+    //короче на данном этапе я могу сделать следующие выводы
+    //out позволяет записать данные в дженерик класс, но поле, в котором будут храниться
+    //данные не доступно для изменения, из него можно только получить информацию
+    //in позволяет записать данные в класс, но получить их мы никак не можем, потому что поле
+    //в котором они хранятся имеет модификатор private
+    //то есть out T это тип T и все его наследники, а in T это тип T и все его родители
+
 }
 
-class LootBox<T : Loot>(vararg item: T) {
-    var open = false
-    private var loot: Array<out T> = item
 
-    fun fetch(item: Int): T? {
-        return loot[item].takeIf { open }
-    }
 
-    operator fun get(index: Int): T? = loot[index].takeIf { open }
-
-    fun <R> fetch(item: Int, lootModFunction: (T) -> R): R? {
-        return lootModFunction(loot[item]).takeIf { open }
-    }
+//к примеру Generic<B> можем присвоить Generic<Base>, но изменить
+public class Generic<out T>(_item: T){//можем только инициализировать переменную единожды и потом читать из нее
+    val item: T = _item
 }
+
+//public class Generic<T>(_item: T){
+//    var item: T = _item
+//}
+
+public open class Base{
+    var baseProperty = "Base property string"
+}
+public class A(_name: String) : Base() {
+
+    public var name: String = _name
+}
+
+public class B(_int: Int) : Base(){
+
+    public var number: Int = _int
+}
+
+class Barrel<in T>(private var item: T)
 
 open class Loot(val value: Int)
 
