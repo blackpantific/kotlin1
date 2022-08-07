ddd424c0447d2e88381021daa2a7804f0a9bcd3b
BlackPantific <blackpantific@gmail.com>
2022-08-07 20:00:15 +0300

    generic restriction

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,32 +1,9 @@
 fun main(args: Array<String>) {
-    val telegram = InstantMessenger("Telegram")
-    val photoCamera = PhotoCamera()
-    val pixel = SmartPhone("Pixel 5", telegram, photoCamera)
-    pixel.send("Hello Kotlin")
-    pixel.takePhoto()
+
 }
 
-//делегирование свойств
-interface Messenger1{
-    val programName: String
+fun <T> getBiggest(a: T, b: T): T where T: Comparable<T>,
+                                        T: Number {
+    return if(a > b) a
+    else b
 }
-class InstantMessenger1(override val programName: String) : Messenger1
-class SmartPhone1(val name: String, m: Messenger1) : Messenger1 by  m
-
-interface Messenger {
-    fun send(message: String)
-}
-
-class InstantMessenger(val programName: String) : Messenger {
-    override fun send(message: String) = println("Send message: `$message`")
-}
-
-interface PhotoDevice {
-    fun takePhoto()
-}
-
-class PhotoCamera : PhotoDevice {
-    override fun takePhoto() = println("Take a photo")
-}
-
-class SmartPhone(val name: String, m: Messenger, p: PhotoDevice) : Messenger by m, PhotoDevice by p
