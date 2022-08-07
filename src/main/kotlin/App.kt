fdf6accdf1e2f9034b59ebf21794092307f116e9
BlackPantific <blackpantific@gmail.com>
2022-08-07 22:10:37 +0300

    delegated properties

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,24 @@
-fun main(args: Array<String>) {
+import kotlin.reflect.KProperty
+
+fun main() {
+
+    val tom = Person("Tom", 37)
+    println(tom.age)    //37
+    tom.age = 38
+    println(tom.age)    //38
+    tom.age = -139
+    println(tom.age)    //38
 
 }
-
-fun <T> getBiggest(a: T, b: T): T where T: Comparable<T>,
-                                        T: Number {
-    return if(a > b) a
-    else b
+class Person(val name: String, _age: Int){
+    var age: Int by LoggerDelegate(_age)
+}
+class LoggerDelegate(private var personAge: Int) {
+    operator fun getValue(thisRef: Person, property: KProperty<*>): Int{
+        return personAge
+    }
+    operator fun setValue(thisRef: Person, property: KProperty<*>, value: Int){
+        println("Устанавливаемое значение: $value")
+        if(value > 0 && value < 110) personAge = value
+    }
 }
