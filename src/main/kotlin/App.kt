977d58a6ef71f79641aa0efd3fa6957fbedeafce
BlackPantific <blackpantific@gmail.com>
2022-09-03 20:12:27 +0300

    lateinit keyword

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -4,32 +4,16 @@ fun main() {
 
 }
 
-class MyClass{
-
-    var p = lazy {
-        "hello world"
+class Player {
+    lateinit var alignment: String
+    fun determineFate() {
+        alignment = "Good"
     }
 
-    var db by getDouble()
-
-    fun getSomeData(): Unit{
-        var c = 5
-        var e = 7
-        println(e + c)
-
+    fun proclaimFate() {
+        //если обратиться к свойству alignment до того как оно будет
+        //инициализированно, то возникнет исключение
+        //в отличие от инициализации при помощи by lazy
+        if (::alignment.isInitialized) println(alignment)
     }
-}
-
-private operator fun Double.setValue(myClass: MyClass, property: KProperty<*>, any: Any) {
-    myClass.db = 100.0
-}
-
-private operator fun Double.getValue(myClass: MyClass, property: KProperty<*>): Any {
-    return 10.0
-}
-
-
-public fun getDouble(): Double{
-    var c = Math.PI
-    return c
 }
