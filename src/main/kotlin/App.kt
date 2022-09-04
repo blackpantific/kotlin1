216042deb5bad78b66a431e14710a7c85248e9f9
BlackPantific <blackpantific@gmail.com>
2022-09-04 20:31:39 +0300

    inheritance & properties

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -5,10 +5,12 @@ import javax.swing.text.View
 
 //КЛАССЫ
 
+
 fun main(args: Array<String>) {
 
     println("Построение класса Derived(\"hello\", \"world\")")
     Derived("hello", "world")
+
 }
 
 //Если у конструктора есть аннотации или
@@ -22,15 +24,15 @@ annotation class Inject
 
 //пример с наследованием и вызовом функции с одинаковым именем из родительского класса
 
-open class Base{
+open class Base {
     var someString = "Hello world"
     var myIntVal: Int
 
-    open fun getInfo(){
+    open fun getInfo() {
         println("Hello world")
     }
 
-    constructor(someInt: Int){
+    constructor(someInt: Int) {
         myIntVal = someInt
     }
 }
@@ -39,14 +41,15 @@ class Inherited : Base {
 
     //может быть первичным конструктором, но в данном примере явно использую слово super для
     //вызова родительского конструктора
-    constructor(someInt: Int) : super(someInt){
+    constructor(someInt: Int) : super(someInt) {
 
     }
-    fun doSmth(){
+
+    fun doSmth() {
         super.getInfo()
     }
 
-    override fun getInfo(){
+    override fun getInfo() {
         println("Hello world 111")
     }
 }
@@ -68,8 +71,12 @@ class Polygon : Shape {
 
 open class Base1(val name: String) {
 
-    init { println("Инициализация класса Base") }
+    init {
+        //вызывается 2м
+        println("Инициализация класса Base")
+    }
 
+    //вызывается 3м
     open val size: Int =
         name.length.also { println("Инициализация свойства size в класса Base: $it") }
 }
@@ -77,10 +84,34 @@ open class Base1(val name: String) {
 class Derived(
     name: String,
     val lastName: String,
-) : Base1(name.replaceFirstChar { it.uppercase() }.also { println("Аргументы, переданные в конструктор класса Base: $it") }) {
+) : Base1(name.replaceFirstChar { it.uppercase() }
+        //вызывается в первую очередь
+    .also { println("Аргументы, переданные в конструктор класса Base: $it") }) {
 
-    init { println("Инициализация класса Derived") }
+    init {
+        //вызывается 4м
+        println("Инициализация класса Derived")
+    }
 
+    //вызывается 5м
     override val size: Int =
         (super.size + lastName.length).also { println("Инициализация свойства size в классе Derived: $it") }
+}
+
+//ОСОБЕННОСТЬ С GET
+
+class Rectangle1(val width: Int, val height: Int) {
+    val area: Int
+        get() = this.width * this.height
+
+    // если в сеттере мы не используем ключ слово field, то инициализировать свойство
+    //каким-то значением не обязательно
+    //КСТАТИ field является тем самым полем в языке котлин, т.е. в данном случае
+    //backing field
+    var stringRepresentation: String
+        get() = this.toString()
+        set(value) {
+            //setDataFromString(value) // парсит строку и устанавливает
+            // значения для других свойств
+        }
 }
