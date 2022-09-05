9c68ad0e68f996b15a35cbb130d75dfee36bce80
BlackPantific <blackpantific@gmail.com>
2022-09-05 21:45:30 +0300

    delegating

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,21 +1,47 @@
 fun main(args: Array<String>) {
 
-    var cl: Outer.Nested = Outer.Nested()
-    cl.foo()
+//    val b: Base = BaseImpl(10)
+//    b.print()
+
+    var d = Derived(BaseImpl(5)).base.print()
+
 }
 
-//просто вложенный класс, аналог статически вложенных классов в джаве
-class Outer {
-    private val bar: Int = 1
-    public class Nested {
-        fun foo() = 2
+interface Base {
+    fun print()
+}
+
+class BaseImpl(val x: Int) : Base {
+    override fun print() { print(x) }
+}
+
+class Derived(private val b: Base) : Base by b  {
+    var base = b
+
+    constructor(val1: Int, b: Base) : this(b)  {
+        base.print()
     }
 }
 
-class Outer1 {
-    private val bar: Int = 1
-    inner class Inner {
-        //обращение к полю из внешнего класса
-        fun foo() = this@Outer1.bar
+interface Human{
+    fun getUserInfo()
+}
+
+open class A(var name: String, var age: Int) : Human{
+    override fun getUserInfo(){
+        println("User name is $name and age is $age")
     }
+}
+
+open class B(name: String, age: Int) : A(name, age){
+
+    var position: String = ""
+    var car: String? = null
+}
+
+class C(private val human: Human, name: String, age: Int) : B(name, age), Human by human{
+    override fun getUserInfo() {
+        human.getUserInfo()
+    }
+
 }
