80fa5f8d25f079804745a5a480b46ea6eab205fe
BlackPantific <blackpantific@gmail.com>
2022-09-05 13:07:52 +0300

    functional interfaces

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,23 +1,42 @@
+import javax.sql.rowset.Predicate
 
 fun main(args: Array<String>) {
 
-}
-
-interface A {
-    fun foo() { print("A") }
-}
-interface B {
-    fun foo() { print("B") }
-}
-
-class C : A, B {
-
-    //интересный пример с реализацией интерфейсов с одинаковыми по имени методами
-    //в шарпе мы бы не могли создать один метод, переопределяющий 2 метода интерфейсов
-    //там были бы 2 метода для каждого интерфейса отдельные
-    override fun foo() {
-        //super<A>.foo()
-        //super<B>.foo()
-        print("foo")
+    var myAction: Action<Int> = Action { val1, val2 ->
+        println(val1 + val2)
     }
+
+    myAction.function(5,7)
+
+    var predicate = object : Predicate1<Int>{
+        override fun compare(val1: Int, val2: Int): Boolean {
+            return val1 > val2
+        }
+    }
+
+    var compareRes = predicate.compare(10, 18)
+
+    var func = Func<Int> { param1, param2 ->
+         param1 + param2
+    }
+
+    func.invokeFun(5, 8)
+
+}
+
+//обычный интерфейс
+interface MyInterface{
+    fun Go()
+}
+
+fun interface Action<T>{
+    fun function(val1: T, val2: T) : Unit
+}
+
+fun interface Predicate1<T>{
+    fun compare(val1: T, val2: T) : Boolean
+}
+
+fun interface Func<T>{
+    fun invokeFun(param1: T, param2: T) : T
 }
