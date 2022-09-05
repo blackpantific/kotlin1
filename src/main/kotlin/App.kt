a20468a285344746eaddffd6ce098f60091aa762
BlackPantific <blackpantific@gmail.com>
2022-09-05 17:44:34 +0300

    generics pt3 variance at the point of use

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,43 +1,77 @@
 fun main(args: Array<String>) {
 
-    val ints: Array<Int> = arrayOf(1, 2, 3)
-    val any = Array<Any>(3) { "" }
-    //невозможно сделать из-за инвариантности
-    copy(ints, any)
+    var a: Generic<A> = Generic<A>(A("1"))
+    a.item = A("2")
+    var aRes = a.get()
+    a.set(A("3"))
+    var s = a.item
+
+    var a1: Generic<in A> = Generic<A>(A("1"))
+    a1.item = A("2")
+    a1.set(A("1"))
+    var sm = a1.get()//вернет Any вместо стринги
+    var s1 = a1.item//снова Any
+    //полный пиздец
+
+    myFun(a)
+
+//    val ints: Array<A> = arrayOf(A("1"))
+//    val any: Array<Base> = arrayOf(Base())
+//    copy(ints, any)
+
+
+    //короче на данном этапе я могу сделать следующие выводы
+    //out позволяет записать данные в дженерик класс, но поле, в котором будут храниться
+    //данные не доступно для изменения, из него можно только получить информацию
+    //in позволяет записать данные в класс, но получить их мы никак не можем, потому что поле
+    //в котором они хранятся имеет модификатор private
+    //то есть out T это тип T и все его наследники, а in T это тип T и все его родители
+
 }
 
-//ВАРИАТИВНОСТЬ НА МЕСТЕ ОБЪЯВЛЕНИЯ
-interface Comparable<in T> {
-    operator fun compareTo(other: T): Int
-}
-
-fun demo(x: Comparable<Number>) {
-    x.compareTo(1.0) // 1.0 имеет тип Double, расширяющий Number
-    // Таким образом, мы можем присвоить значение x переменной типа Comparable<Double>
-    val y: Comparable<Double> = x // OK!
-}
-
-fun copy(from: Array<Any>, to: Array<Any>) {
+fun copy(from: Array<Base>, to: Array<Base>) {
     assert(from.size == to.size)
-    for (i in from.indices)
+    for (i in from.indices) {
         to[i] = from[i]
+    }
 }
 
-//ВАРИАТИВНОСТЬ НА МЕСТЕ ИСПОЛЬЗОВАНИЯ
+fun myFun(someVal: Generic<out Base>){
+    var s = someVal.item
+    someVal.item = A("1")
+}
 
-//Произошедшее здесь называется проекция типов:
-//мы сказали, что from — не просто массив, а ограниченный
-//(спроецированный): мы можем вызывать только те методы,
-//которые возвращают параметризованный тип T, что в этом случае
-//означает, что мы можем вызывать только get(). Таков наш подход к вариативности
-//на месте использования, и он соответствует Array<? extends Object> из Java, но в более простом виде.
-fun copy1(from: Array<out Any>, to: Array<Any>) { }
+fun fill(dest: Generic<in String>, value: String){
 
-class Array<T>(val size: Int) {
+    dest.item = value
 
-    //если мы пишем Array<out Any>, то мы не можем вызвать метод set
-    operator fun get(index: Int): T {  }
+    //вернет значение, но не стринг, как ожидалось, а тип Any() ахуеть просто
+    var v = dest.item
+}
 
-    //если мы пишем Array<in Any>, то мы не можем вызвать метод get
-    operator fun set(index: Int, value: T) { }
+public class Generic<T>(_item: T) {
+
+    var item: T = _item
+
+    fun get() : T{
+        return item
+    }
+
+    fun set(value: T){
+        item = value
+    }
+}
+
+public open class Base {
+    var baseProperty = "Base property string"
+}
+
+public class A(_name: String) : Base() {
+
+    public var name: String = _name
+}
+
+public class B(_int: Int) : Base() {
+
+    public var number: Int = _int
 }
