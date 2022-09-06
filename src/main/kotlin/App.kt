ef6f470877e629e93a6df447d498fdcdcd959b82
BlackPantific <blackpantific@gmail.com>
2022-09-06 20:26:15 +0300

    delegating in details

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,47 +1,70 @@
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
 fun main(args: Array<String>) {
 
 //    val b: Base = BaseImpl(10)
 //    b.print()
 
-    var d = Derived(BaseImpl(5)).base.print()
+    var ex = Example()
+    var res = ex.p
+    ex.p = "qwerty"
 
+    var c = 4
+
+    val user = User()
+    user.name = "first"
+    user.name = "second"
 }
 
-interface Base {
-    fun print()
+class Example {
+    var p: String by Delegate()
 }
 
-class BaseImpl(val x: Int) : Base {
-    override fun print() { print(x) }
-}
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
 
-class Derived(private val b: Base) : Base by b  {
-    var base = b
-
-    constructor(val1: Int, b: Base) : this(b)  {
-        base.print()
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name}' in $thisRef.")
     }
 }
 
-interface Human{
-    fun getUserInfo()
+//мои попытки создать метод, возвращающий значение(метод ниже)
+val lazyValue: String by getstr<String>()
+
+private operator fun String.getValue(nothing: Nothing?, property: KProperty<*>): String {
+    return "gfgf"
 }
 
-open class A(var name: String, var age: Int) : Human{
-    override fun getUserInfo(){
-        println("User name is $name and age is $age")
+//в интерфейсе Lazy уже реализован метод getValue, который всегда требуется при делегировании свойств
+fun <T> getstr(): String /*Lazy<String>*/{
+    return "12345678"
+}
+
+//инвокаем метод при каждом присваивании новых значений
+
+class User {
+    var name: String by Delegates.observable("<no name>") {
+            prop, old, new ->
+        println("$old -> $new")
     }
 }
 
-open class B(name: String, age: Int) : A(name, age){
+class Resource
 
-    var position: String = ""
-    var car: String? = null
+class Owner {
+    var varResource: Resource by ResourceDelegate()
 }
 
-class C(private val human: Human, name: String, age: Int) : B(name, age), Human by human{
-    override fun getUserInfo() {
-        human.getUserInfo()
+class ResourceDelegate(private var resource: Resource = Resource()) {
+    operator fun getValue(thisRef: Owner, property: KProperty<*>): Resource {
+        return resource
     }
-
+    operator fun setValue(thisRef: Owner, property: KProperty<*>, value: Any?) {
+        if (value is Resource) {
+            resource = value
+        }
+    }
 }
