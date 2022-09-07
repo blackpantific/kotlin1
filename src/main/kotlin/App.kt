8f29111d9a1ddff1015a0ade41297598da2464e2
BlackPantific <blackpantific@gmail.com>
2022-09-07 19:24:42 +0300

    infix function

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,27 +1,20 @@
-import kotlin.properties.Delegates
-import kotlin.reflect.KProperty
-
-//ПСЕВДОНИМЫ ТИПОВ
-
-typealias IntSet = Set<Int>
-
-typealias FileTable<K> = MutableMap<K, MutableList<String>>
-
-typealias MyHandler = (Int, String, Any) -> Unit
-
-typealias Predicate<T> = (T) -> Boolean
-
-class A {
-    inner class Inner
-}
-class B {
-    inner class Inner
-}
-
-typealias AInner = A.Inner
-typealias BInner = B.Inner
-
 fun main(args: Array<String>) {
 
+}
 
+//обязателен 1 параметр в ф-ции, который является аргументом справа от функции, а
+//объект у которого вызывается функция, находится слева, то есть
+// 5 multipleVal 7 это 5.multipleVal(7)
+public infix fun Int.multipleVal(x: Int): Int{
+    return this * x
+}
+
+class MyStringCollection {
+    infix fun add(s: String) { /*...*/ }
+
+    fun build() {
+        this add "abc" // Верно
+        add("abc")     // Верно
+        //add "abc"    // Не верно: получатель должен быть указан
+    }
 }
