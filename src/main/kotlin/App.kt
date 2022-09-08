74b039ae37a0a3dfc72e69dfe521f6252617e24f
BlackPantific <blackpantific@gmail.com>
2022-09-08 12:42:57 +0300

    lambda closures

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,20 +1,14 @@
 fun main(args: Array<String>) {
 
-}
+    var c = 5
 
-//обязателен 1 параметр в ф-ции, который является аргументом справа от функции, а
-//объект у которого вызывается функция, находится слева, то есть
-// 5 multipleVal 7 это 5.multipleVal(7)
-public infix fun Int.multipleVal(x: Int): Int{
-    return this * x
-}
-
-class MyStringCollection {
-    infix fun add(s: String) { /*...*/ }
-
-    fun build() {
-        this add "abc" // Верно
-        add("abc")     // Верно
-        //add "abc"    // Не верно: получатель должен быть указан
+    var lambda: () -> Int = {
+        var r = c
+        c = 15
+        r
     }
+
+    lambda.invoke()
+
+    var r = c
 }
