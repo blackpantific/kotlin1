42010a8839fa0029d99a05db58a183d74ae5ed26
BlackPantific <blackpantific@gmail.com>
2022-09-08 16:54:58 +0300

    'this' keyword

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,14 +1,27 @@
 fun main(args: Array<String>) {
 
+    var b: A.B = A().B()
+    5.foo()
+}
+
+class A { // неявная метка @A
     var c = 5
+    inner class B { // неявная метка @B
+        fun Int.foo() { // неявная метка @foo
+            val a = this@A // this из A
+            val b = this@B // this из B
 
-    var lambda: () -> Int = {
-        var r = c
-        c = 15
-        r
+            val c = this // объект-приёмник функции foo(), типа Int
+            val c1 = this@foo // объект-приёмник функции foo(), типа Int
+
+            val funLit = lambda@ fun String.() {
+                val d = this // объект-приёмник литерала funLit
+            }
+
+            val funLit2 = { s: String ->
+                // объект-приёмник функции foo(), т.к. замыкание лямбды не имеет объекта-приёмника
+                val d1 = this
+            }
+        }
     }
-
-    lambda.invoke()
-
-    var r = c
 }
