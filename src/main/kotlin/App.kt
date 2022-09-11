709c46413c2588af13f45ad5edb762ad87532218
BlackPantific <blackpantific@gmail.com>
2022-09-11 18:42:11 +0300

    async flow

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,29 +1,33 @@
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-suspend fun main() = coroutineScope{
-
-    val channel = Channel<Int>()
-    launch {
-        for (n in 1..5) {
-            // отправляем данные через канал
-            channel.send(n)
-        }
-        channel.close()
-    }
-
-    // получаем данные из канала
-    for(user in channel) {  // Получаем данные из канала
-        println(user)
-    }
-    println("End")
+suspend fun main() {
+    //getUsers().collect { user -> println(user) }//collect-это функция, определенная в интерфейсе Flow
+    //Стоит отметить, что асинхронный поток не запускается,
+    // пока не будет применена терминальная операция над получаемыми даными, например, функция collect()
+    val numberFlow = getNumbers()       // поток создан, но не запущен
+    println("numberFlow has created")
+    println("launch collect function")
+    numberFlow.collect { number -> println(number) }   // запуск потока
+    delay(5000)//до этого места поток main заморожен пока не получит все данные
 }
 
-fun CoroutineScope.getUsers(): ReceiveChannel<String> = produce{
-    val users = listOf("Tom", "Bob", "Sam")
-    for (user in users) {
-        send(user)
+fun getUsers(): Flow<String> = flow {
+    val database = listOf("Tom", "Bob", "Sam")  // условная база данных
+    var i = 1;
+    for (item in database) {
+        delay(400L) // имитация продолжительной работы
+        println("Emit $i item")
+        emit(item) // емитируем значение
+        i++
+    }
+}
+
+fun getNumbers() = flow{
+    println("numberFlow has started")
+    for(item in 1..5){
+        delay(400L)
+        emit(item * item)
     }
 }
