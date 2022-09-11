e98b574c1a571635c034c739c3bcd61712a5c377
BlackPantific <blackpantific@gmail.com>
2022-09-11 17:47:24 +0300

    cancellation coroutines

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,59 +1,19 @@
 import kotlinx.coroutines.*
 
-//ПОСЛЕДОВАТЕЛЬНОСТЬ ИСПОЛНЕНИЯ ЕЩЕ РАЗ
-//suspend fun main() {
-//    var c= 5
-//    coroutineScope {
-//
-//        async{ printHello()}
-//        println("Program has finished")//выполняется 2м
-//    }
-//
-//    println("some output")//выполняется 1м
-//}
-//
-//suspend fun printHello(){
-//    delay(500L)  // имитация продолжительной работы
-//    println("Hello work!")//выполняется 3м
-//}
+suspend fun main() = coroutineScope {
 
-//suspend fun main() = coroutineScope{
-//
-//    launch {
-//        println("Корутина выполняется на потоке: ${Thread.currentThread().name}")
-//    }
-//    println("Функция main выполняется на потоке: ${Thread.currentThread().name}")
-//}
-
-//Конфигурация Dispatchers.Default
-//suspend fun main() = coroutineScope{
-//
-//    launch(Dispatchers.Default) {   // явным образом определяем диспетчер Dispatcher.Default
-//        println("Корутина выполняется на потоке: ${Thread.currentThread().name}")
-//    }
-//    println("Функция main выполняется на потоке: ${Thread.currentThread().name}")
-//}
-
-//Dispatchers.Unconfined
-//suspend fun main() = coroutineScope{
-//
-//    launch(Dispatchers.Unconfined) {
-//        println("Поток корутины (до остановки): ${Thread.currentThread().name}")//выполняется в main
-//        //до первой остановки, напоминает async/await в c# с продолжением выполнения в другом потоке
-//        //после выполнения await, а до этого метод вызывается и выполняется в том же потоке до оператора await
-//        delay(500L)
-//        println("Поток корутины (после остановки): ${Thread.currentThread().name}")
-//    }
-//
-//    println("Поток функции main: ${Thread.currentThread().name}")
-//}
-
-//newSingleThreadContext("Custom Thread") ручное создание потока с заданным именем
-suspend fun main() = coroutineScope{
-
-    launch(newSingleThreadContext("Custom Thread")) {
-        println("Поток корутины: ${Thread.currentThread().name}")
+    val downloader = async {
+        println("Начинаем загрузку файлов")
+        for (i in 1..5) {
+            println("Загружен файл $i")
+            delay(500L)
+        }
     }
-
-    println("Поток функции main: ${Thread.currentThread().name}")
+    delay(800L)     // установим задержку, чтобы несколько файлов загрузились
+    println("Надоело ждать, пока все файлы загрузятся. Прерву-ка я загрузку...")
+    downloader.cancelAndJoin()    // отменяем корутину
+    downloader.await()//если мы используем async/await, то при отмене асинка надо использовать блок try или
+    //вылетит исключение
+    // ожидаем завершения корутины
+    println("Работа программы завершена")
 }
